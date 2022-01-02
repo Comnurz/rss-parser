f8ed4b2a8ac5c0d55a8fd6adb1e75a3a6483c534
Caglanur Saglam <slbandidas@gmail.com>
2022-01-02 22:53:19 +0300

    added router and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 # rss-parser
+
+## Requirements
+* Python = 3.9
+* [Docker](https://www.docker.com/)
+
+## Installation
+
+### Docker way
+
+Build Docker image
+```sh
+$ docker build -t rss:latest .
+```
+
+Run Console via Docker
+```sh
+$ docker run -p 8080:8080 rss:latest .
+```
+
+### Local Environment Way
+
+Create virtual environment & activate it
+```shell
+$ python3.9 -m venv 'venv-example' 
+$ source venv-example/bin/activate
+```
+Install the requirements
+```shell
+$ pip install -r src/requirements.txt
+```
+
+You can run the app programmatically from main.py or with uvicorn.
+
+1.Programmatically, add this to your main.py and run it.
+```python
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="host", port="port", log_level="info")
+```
+2. Uvicorn 
+```shell
+uvicorn main:app --reload --host "host" --port "port"
+```
+
 ### Code Styling
 
 #### Flake8 and Black with pre-commit
@@ -6,13 +49,13 @@
 1. Installation:
  - with pip
     ```sh
-    cd path/to/rent-flex-mvp/
+    cd path/to/rss-parser/
     python<version> -m pip install pre-commit
     python<version> -m pre-commit install
     ```
  - with brew on OsX
     ```sh
-    cd path/to/rent-flex-mvp/
+    cd path/to/rss-parser/
     brew install pre-commit
     pre-commit install
     ```
--- src/main.py
+++ src/main.py
@@ -1,13 +1,82 @@
-from fastapi import FastAPI
+from datetime import timedelta
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from starlette.responses import Response
+from fastapi.security import OAuth2PasswordRequestForm
+
+from models import models
+from models.schemas import Token
+from utils.database import engine
+from api.router import api_router
+from utils.database import get_db
+from helpers.authenticator import authenticate_user, create_access_token
+from helpers.security import ACCESS_TOKEN_EXPIRE_MINUTES
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(api_router)
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    """
+    Get Token via email and password
+
+    :param form_data: OAuth2PasswordRequestForm
+    :param db: Session
+    :return: Token
+    """
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    """
+    Welcome to route.
+
+    :return: Dict[str, str]
+    """
+    return {"message": "Hello World!!"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/ready", summary="k8s Ready")
+def ready() -> Response:
+    """
+    API for internal kubernetes.
+
+    :return: Response
+    """
+    return Response(status_code=status.HTTP_200_OK, content="Success")
+
+
+@app.get("/healthz", summary="k8s Healthz")
+def healthz() -> Response:
+    """
+    API for internal kubernetes.
+
+    :return: Response
+    """
+    return Response(status_code=status.HTTP_200_OK, content="Success")
+
+
+# import uvicorn
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8888)
